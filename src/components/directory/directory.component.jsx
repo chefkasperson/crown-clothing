49dd44edb14f9718abb285c59f1ffddd86de1e4d
chefkasperson <chefkasperson@gmail.com>
2020-08-19 23:20:16 -0700

    added stylesheet links

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import MenuItem from '../menu-item/menu-item.component'
+import './directory.styles.scss'
 
 
 class Directory extends React.Component {
@@ -47,7 +48,14 @@ class Directory extends React.Component {
     render() {
         return(
             <div className="directory-menu">
+                {
+                    this.state.sections.map({title, imageUrl, id} => (
+                        <MenuItem key={title} title={title} />
+                    ))
+                }
             </div>
         )
     }
 }
+
+export default Directory
