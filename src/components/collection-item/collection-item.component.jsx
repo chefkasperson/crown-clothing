444c0aab4638ccb27daccb5372c06d64191d9272
chefkasperson <chefkasperson@gmail.com>
2020-08-23 23:33:54 -0700

    fixed content-footer typos, added padding to entire project

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -9,7 +9,11 @@ const CollectionItem = ({name, price, id, imageUrl}) => (
                 backgroundImage: `url(${imageUrl})`
             }}
             />
-            <div className="collection-footer"></div>
+            <div className="collection-footer">
+            <span className="name">{name}</span>
             <span className="price">{price}</span>
+            </div>
     </div>
 )
+
+export default CollectionItem
--- src/components/preview-collection/collection-preview.component.jsx
+++ src/components/preview-collection/collection-preview.component.jsx
@@ -10,7 +10,7 @@ const CollectionPreview = ({title, items}) => (
                 items
                 .filter((item, idx) => idx < 4)
                 .map(({id, ...otherItemProps}) => (
-                    <CollectionItem key={id} {...otherItemProps} /></CollectionItem>
+                    <CollectionItem key={id} {...otherItemProps} />
                 ))
             }
         </div>
