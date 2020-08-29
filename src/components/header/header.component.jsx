f39a0bc04b4f7b4efb5e2b84dfd4f9f8325a7aa1
chefkasperson <chefkasperson@gmail.com>
2020-08-29 01:02:17 -0700

    added cart dropdown container and redux for it

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,6 +5,7 @@ import {connect} from 'react-redux'
 import { ReactComponent as Logo} from '../../assets/crown.svg'
 import {auth} from '../../firebase/firebase.utils'
 import CartIcon from '../cart-icon/cart-icon.component'
+import CartDropdown from '../cart-dropdown/cart-dropdown.component'
 
 
 const Header = ({ currentUser }) => (
@@ -27,6 +28,7 @@ const Header = ({ currentUser }) => (
             }
             <CartIcon />
         </div>
+        <CartDropdown />
     </div>
 )
 
