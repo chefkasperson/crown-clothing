4200c3f8dbe3a5c7a3436273897143dfa1e60cd9
chefkasperson <chefkasperson@gmail.com>
2020-09-02 01:10:35 -0700

    added styles to checkout page

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -4,7 +4,23 @@ import './checkout.styles.scss'
 
 const CheckoutPage = () => (
     <div className="checkout-page">
-        checkout page
+        <div className="checkout-header">
+            <div className="header-block">
+                <span>Product</span>
+            </div>
+            <div className="header-block">
+                <span>Description</span>
+            </div>
+            <div className="header-block">
+                <span>Quantity</span>
+            </div>
+            <div className="header-block">
+                <span>Price</span>
+            </div>
+            <div className="header-block">
+                <span>Remove</span>
+            </div>
+        </div>
     </div>
 )
 
