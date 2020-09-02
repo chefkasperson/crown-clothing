c84c89c2c1dff113241cc5389198d29c62533b54
chefkasperson <chefkasperson@gmail.com>
2020-09-02 00:42:17 -0700

    converted all to use reselect selectors

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,8 +4,11 @@ import { Link } from 'react-router-dom'
 import {connect} from 'react-redux'
 import { ReactComponent as Logo} from '../../assets/crown.svg'
 import {auth} from '../../firebase/firebase.utils'
+import {selectCartHidden} from '../../redux/cart/cart.selectors'
+import { selectCurrentUser} from '../../redux/user/user.selectors'
 import CartIcon from '../cart-icon/cart-icon.component'
 import CartDropdown from '../cart-dropdown/cart-dropdown.component'
+import { createStructuredSelector } from 'reselect'
 
 
 const Header = ({ currentUser, hidden }) => (
@@ -34,9 +37,10 @@ const Header = ({ currentUser, hidden }) => (
     </div>
 )
 
-const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
-    currentUser: currentUser,
-    hidden: hidden
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser,
+    hidden: selectCartHidden
 })
+
 
 export default connect(mapStateToProps)(Header)
