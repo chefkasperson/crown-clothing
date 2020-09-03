6b1bf95dfb5cd0b90db78c679c365816e9c4cbee
chefkasperson <chefkasperson@gmail.com>
2020-09-03 00:27:48 -0700

    moved shop directory state to redux

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -3,7 +3,7 @@ import {persistReducer} from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 
 import cartReducer from './cart/cart.reducer'
-
+import directoryReducer from './directory/directory.reducer'
 import userReducer from './user/user.reducer'
 
 const persistConfig = {
@@ -14,7 +14,8 @@ const persistConfig = {
 
 const rootReducer = combineReducers({
     user: userReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    directory: directoryReducer
 })
 
 export default persistReducer(persistConfig, rootReducer)
