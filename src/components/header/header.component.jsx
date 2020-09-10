8e92610108ea498bc3aafbd91f29b56373b2dacf
chefkasperson <chefkasperson@gmail.com>
2020-09-09 23:02:11 -0700

    removed css block and converted optiondiv from optionLin

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -10,7 +10,7 @@ import CartIcon from '../cart-icon/cart-icon.component'
 import CartDropdown from '../cart-dropdown/cart-dropdown.component'
 import { createStructuredSelector } from 'reselect'
 
-import { HeaderContainer, LogoContainer, OptionsContainer, OptionDiv, OptionLink } from './header.styles'
+import { HeaderContainer, LogoContainer, OptionsContainer, OptionLink } from './header.styles'
 
 
 const Header = ({ currentUser, hidden }) => (
@@ -27,7 +27,7 @@ const Header = ({ currentUser, hidden }) => (
             </OptionLink>
             {
                 currentUser ?
-                <OptionDiv onClick={() => auth.signOut()} >SIGN OUT</OptionDiv>
+                <OptionLink as='div' onClick={() => auth.signOut()} >SIGN OUT</OptionLink>
                 :
                 <OptionLink to='/signin' >SIGN IN</OptionLink>
             }
